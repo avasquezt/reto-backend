ea1b88ffea85a4bfdff5a11f9e661f52b58937e5
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-12-02 22:10:18 -0500

    Change fetch type to lazy in affiliate and test columns

--- reto_backend/src/main/java/com/reto_backend/reto_backend/model/Appointment.java
+++ reto_backend/src/main/java/com/reto_backend/reto_backend/model/Appointment.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.OnDeleteAction;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -39,12 +40,12 @@ public class Appointment {
     @Column(name = "hours")
     private LocalTime hour;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_test")
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Test test;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_affiliate")
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Affiliate affiliate;
